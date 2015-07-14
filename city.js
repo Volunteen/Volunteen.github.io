d1b134e9428ea4b7400140d61ee578d75e135d4d
dianaguan <bubbilybubbily@gmail.com>
2015-07-14 11:15:53 -0700

    Update city.js

--- city.js
+++ city.js
@@ -3,47 +3,49 @@ var map = L.mapbox.map('map', 'examples.map-20v6611k')
   .setView([38.12367, -76.81229], 9);
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
-//var spreadsheet_url = '1B93XRI8b9NB_KiK7pSBz6C6FQeAVqRX3gebrOkNap_s';
-var spreadsheet_url = '1JcWAS8ZRwlbDb1brMHI87NfRm5hI8F-BBafuWKu3OOc';
-var spreadsheet_urlResponses = '1KA9NfxB0fByKWIsXSm8jEzIiOzlhtJRcuPLWZUPSREA';
+var spreadsheet_url = '1rrsELAXKgLn6DMjNUsb721NmsRu4dqdDPAjkdPFkVnQ';
 
-window.onload = function () {
-	Tabletop.init ( {
-		key: spreadsheet_url,
-		key: spreadsheet_urlResponses,
-		callback: showInfo,
-		simpleSheet: true
-	} );
+
+window.onload = function(){
+	Tabletop.init(   {  key: spreadsheet_url,
+						callback: showInfo, 
+						simpleSheet: true	
+				  }  );
 };
 
-function showInfo(data, tabletop) {
-	//alert("Successfully processed!");
-	//console.log(data);
-	
-	var places=[];
-	for (var i=0; i<data.length; i++){
-		var place = data[i];
-		console.log(place);
-		
-		var placeJSON = { type: 'Feature',
-						properties: {
-						title: place.name,
-						description: place.description,
-						// image: place.image,
-						'marker-color':place.color,
-						'marker-size': 'large',
-						'marker-symbol': place.symbol,
-						},
-						geometry: {
-							type: 'Point',
-							coordinates: [place.longitude, place.latitude]
-						}
-		};
-		console.log(placeJSON);
-		places.push(placeJSON);
-	}
-	setupMap(places);
-}	
+function showInfo(data, Tabletop) {
+    alert("Successfully processed!")
+    console.log(data);
+    
+    var places = [];
+    for(var i= 0; i< data.length; i++){
+    	var place = data[i];
+    	// console.log(place);
+    	
+    	var placeJSON = { type: 'Feature',
+    					properties: {
+    						title: place.name,
+    						description: place.description,
+    						'marker-color': place.color,
+    						'marker-size': 'large',
+    						'marker-symbol': place.symbol,
+    					},
+    					
+    					geometry : {
+    						type: 'Point',
+    						coordinates: [place.longitude, place.latitude]
+    					}
+    	
+    				};
+    				
+    	console.log(placeJSON);
+    	places.push(placeJSON);
+    }
+    
+    setupMap(places);
+
+}    
+
 
 function setupMap(placeList){
 	myLayer.setGeoJSON(placeList);
