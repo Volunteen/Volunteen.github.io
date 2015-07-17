d24f9ea62594735cdb949ccdbe17b4ba6f6137a9
MSkall <marmarskall@gmail.com>
2015-07-17 15:37:57 -0700

    Update city.js

--- city.js
+++ city.js
@@ -2,7 +2,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiZ3NmZ2lybHN3aG9jb2RlIiwiYSI6IjA0MmYzMjVkNTRlM
 var map = L.mapbox.map('map', 'mapbox.streets')
   .setView([38.12367, -76.81229], 9);
 
-var myLayer = L.mapbox.featureLayer().addTo(map);
+var myLayer = L.mapbox.featureLayer();
 
 var spreadsheet_url = '1O3dYsn5WFju_t-AgDR-FwEtGGpKLLc5rfyn89aqrD24';
 
