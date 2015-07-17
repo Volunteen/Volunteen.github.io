4c1e90cf33835ca2cdcb98f2d22d107e8d8fe393
dianaguan <bubbilybubbily@gmail.com>
2015-07-17 13:58:27 -0700

    Update mapotherevents.js

--- mapotherevents.js
+++ mapotherevents.js
@@ -24,7 +24,7 @@ function showInfo(data, Tabletop) {
     						title: place.name,
     						description: place.description,
     						type:place.type,
-    						description: place.description,
+    						description: place.description + "br" + place.link,
     						'marker-color': place.color,
     						'marker-size': 'large',
     						'marker-symbol': place.symbol,
