6d3bff194f5aa4a05e259e67da835b0c8e1c521d
MSkall <marmarskall@gmail.com>
2015-07-20 12:41:55 -0700

    Update mapanimals.js

--- mapanimals.js
+++ mapanimals.js
@@ -2,7 +2,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiZ3NmZ2lybHN3aG9jb2RlIiwiYSI6IjA0MmYzMjVkNTRlM
 var map = L.mapbox.map('map', 'examples.map-20v6611k')
   .setView([38.12367, -76.81229], 9);
 
-var myLayer = L.mapbox.featureLayer().addTo(map);
+var myLayer = L.mapbox.featureLayer();
 var spreadsheet_url = '1gf2ZaQQz-CEPMaaIKW81vFs-fVHoZ55-hXOEU61aguk';
 
 
