efb1771a90d18df711372a73a07289d1e6b5719a
MSkall <marmarskall@gmail.com>
2015-07-20 10:31:26 -0700

    Update city.js

--- city.js
+++ city.js
@@ -4,7 +4,8 @@ var map = L.mapbox.map('map', 'mapbox.streets')
 
 var myLayer = L.mapbox.featureLayer();
 
-var spreadsheet_url = '1O3dYsn5WFju_t-AgDR-FwEtGGpKLLc5rfyn89aqrD24';
+var spreadsheet_url = '1WbhX6t8p17M1M74uFPgKJplZi_wuCkUvLFGjDKYIL54';
+//1O3dYsn5WFju_t-AgDR-FwEtGGpKLLc5rfyn89aqrD24';
 //1O3dYsn5WFju_t-AgDR-FwEtGGpKLLc5rfyn89aqrD24';
 
 window.onload = function(){
